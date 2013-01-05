7536d05f906382e833a6f4d57da298b97bc1f3b9
Bobby Ren <bobbyren@gmail.com>
2013-01-04 18:46:54 -0800

    Initial Commit

---
+++ Camera Framework App/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Camera Framework App
+//
+//  Created by Bobby Ren on 1/4/13.
+//  Copyright (c) 2013 Neroh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
---
+++ Camera Framework App/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Camera Framework App
+//
+//  Created by Bobby Ren on 1/4/13.
+//  Copyright (c) 2013 Neroh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
